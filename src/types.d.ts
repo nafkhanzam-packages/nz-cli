d6638fd893b3a164b619c26a2410f0a64dd45254
nafkhanzam <nafkhanalzamzami@gmail.com>
2021-06-24 01:40:51 +0700

    refactor to default directly

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,6 +1,4 @@
 declare module "deep-sort-object" {
   // eslint-disable-next-line no-unused-vars
-  function _default<T>(a: T): T;
-
-  export = _default;
+  export default function <T>(a: T): T;
 }
